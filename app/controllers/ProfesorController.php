a433f92ab77d3901f4841aff2c2a7d97659e6472
jrivera294 <jrivera294@gmail.com>
2015-01-05 21:29:58 -0430

    asistencias profesor funcionando

--- app/controllers/ProfesorController.php
+++ app/controllers/ProfesorController.php
@@ -9,7 +9,6 @@ class ProfesorController extends BaseController {
 
     public function materias()
 	{
-
         $materias = Seccion::getSeccionesProfesor(Auth::user()->id);
 
 		return View::make('pages/profesor/materias')->with('materias', $materias);
@@ -21,12 +20,50 @@ class ProfesorController extends BaseController {
         $fechas = Horarios::where('seccion_id','=',$id)->get();
 
         if($fecha!=null){
-            $alumnos = Seccion::getAlumnosSeccion($id);
+            $alumnos = Seccion::getAlumnosSeccion($id,$fecha);
         }
 
         return View::make('pages/profesor/asistencias')
             ->with('alumnos', $alumnos)
-            ->with('fechas', $fechas);
+            ->with('fechas', $fechas)
+            ->with('seccion_id',$id)
+            ->with('fecha_id',$fecha);
 	}
 
+    public function asistenciasPost(){
+        $asistencias = Input::except(array('_token', 'seccion_id','horario_id'));
+        foreach ($asistencias as $user_id => $asistencia){
+            if($asistencia!=(-1)){
+                $data = Usuarios_tienen_asistencias::where('user_id','=',$user_id)
+                    ->where('seccion_id','=',Input::get('seccion_id'))
+                    ->where('horario_id','=',Input::get('horario_id'))->first();
+                if(is_null($data)){
+                    $alumno = new Usuarios_tienen_asistencias;
+                    $data = array(
+                        'user_id' => $user_id,
+                        'asistencia' => $asistencia,
+                        'seccion_id' => Input::get('seccion_id'),
+                        'horario_id' => Input::get('horario_id')
+                    );
+                    $alumno->fill($data);
+                }else{
+                    $alumno = Usuarios_tienen_asistencias::find($data->id);
+                    $alumno->asistencia = $asistencia;
+                }
+                $alumno->save();
+            }
+        }
+
+        return Redirect::route('profesor.asistencias',array(Input::get('seccion_id'),Input::get('horario_id')))
+            ->with('mensaje_error', 'Asistencias almacenadas correctamente')
+            ->with('tipo_error', 'success');
+    }
+
+    public function asistenciasTodos($id,$fecha,$asistencia){
+        return $asistencia;
+
+        return Redirect::route('profesor.asistencias',array(Input::get('seccion_id'),Input::get('horario_id')))
+            ->with('mensaje_error', 'Asistencias almacenadas correctamente')
+            ->with('tipo_error', 'success');
+    }
 }
--- app/models/Carrera.php
+++ app/models/Carrera.php
@@ -5,7 +5,7 @@ use Illuminate\Auth\UserInterface;
 use Illuminate\Auth\Reminders\RemindableTrait;
 use Illuminate\Auth\Reminders\RemindableInterface;
 
-class Seccion extends Eloquent implements UserInterface, RemindableInterface {
+class Carrera extends Eloquent implements UserInterface, RemindableInterface {
 
 	use UserTrait, RemindableTrait;
 
