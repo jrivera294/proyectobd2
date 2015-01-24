a57def311597603b8ba1cb2c61b8a8beda3c41ed
jrivera294 <jrivera294@gmail.com>
2015-01-23 20:46:55 -0430

    merge

--- app/controllers/DirectorController.php
+++ app/controllers/DirectorController.php
@@ -17,7 +17,7 @@ class DirectorController extends BaseController {
         return View::make('pages/director/materias/tablaProfesores')->with('profesores',$profesores)->with('id',$id)->with('periodo',$periodo);
 
     }
-        
+
     public function  asignar_profesor ()
     {
 
@@ -127,18 +127,18 @@ class DirectorController extends BaseController {
     }
 
          public function secciones($materia_id) {
-            $seccion = Seccion::getSeccionesMateria($materia_id); 
+            $seccion = Seccion::getSeccionesMateria($materia_id);
             $materia = Materia::find($materia_id);
             return View::make('pages/director/seccion/secciones_materia')->with('seccion',$seccion)->with('materia',$materia);
          }
-            
+
        public function eliminarSeccion($materias_id,$id){
             $seccion = Seccion::find($id);
 
             $seccion->delete();
 
             return Redirect::to('director/materias/'.$materias_id.'/secciones/');
-       } 
+       }
 
 
     public function alumnosMateriasPerdidas(){
@@ -212,7 +212,7 @@ class DirectorController extends BaseController {
            // return $data ;
 
             foreach ($data['alumno_id'] as $datos)
-            {    
+            {
                User::agregar_Alumnos($datos, $data['seccion']);
             }
             $seccion = Seccion::find($data['seccion']);
